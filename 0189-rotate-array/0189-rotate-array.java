1fab1b5e4e1ea8d18d3ef1796142bd35c33f4fb1
Tanay Singh <tanay8705@gmail.com>
2023-08-26 19:37:13 +0530

    Time: 1 ms (50.04%), Space: 56.7 MB (6.58%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -2,26 +2,19 @@ class Solution {
     public void rotate(int[] nums, int k) {
         k = k%nums.length;
         int l = 0,r = nums.length-1;
-        while(l<r){
-            nums[l] = nums[l]^nums[r];
-            nums[r] = nums[l]^nums[r];
-            nums[l] = nums[l]^nums[r];
-            l++;
-            r--;
-        }
+        helper(nums,l,r);
         
         l = 0;
         r = k-1;
-        while(l<r){
-            nums[l] = nums[l]^nums[r];
-            nums[r] = nums[l]^nums[r];
-            nums[l] = nums[l]^nums[r];
-            l++;
-            r--;
-        }
+        helper(nums,l,r);
         
         l = k;
         r = nums.length-1;
+        helper(nums,l,r);
+        
+    }
+    
+    public void helper(int[] nums,int l, int r){
         while(l<r){
             nums[l] = nums[l]^nums[r];
             nums[r] = nums[l]^nums[r];
@@ -29,6 +22,5 @@ class Solution {
             l++;
             r--;
         }
-        
     }
 }
