817d0d9a61d9695c007eb4c4c8e931474e94c7dd
Tanay Singh <tanay8705@gmail.com>
2023-08-27 22:33:32 +0530

    Time: 3 ms (17.53%), Space: 41.5 MB (7.07%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        for(int i = 0;i<citations.length;i++){
+            if(citations[i]>=citations.length-i){
+                System.gc();
+                return citations.length-i;
+            }
+        }
+        System.gc();
+        return 0;
+    }
+}
