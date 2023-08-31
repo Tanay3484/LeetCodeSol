99b6bc682424ba2f0b9c00be540b41c336233345
Tanay Singh <tanay8705@gmail.com>
2023-08-31 17:11:49 +0530

    Time: 1 ms (23.34%), Space: 41.3 MB (99.99%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -17,18 +17,16 @@ class Solution {
 
     public boolean isValidBST(TreeNode root) {
         if (root == null) return true;
+        System.gc();
         return dfs(root, null, null);
     }
 
     private boolean dfs(TreeNode root, Integer min, Integer max) {
         if (root == null) return true;
 
-        if (
-            (min != null && root.val <= min) || max != null && root.val >= max
-        ) {
+        if ((min != null && root.val <= min) || (max != null && root.val >= max)) {
             return false;
         }
-
         return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
     }
 }
