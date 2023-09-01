ea341412a38a80e83316f42b08871480b9fdc051
Tanay Singh <tanay8705@gmail.com>
2023-09-01 12:45:21 +0530

    Time: 41 ms (43.97%), Space: 55.3 MB (22.07%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+
+class TrieNode {
+    public HashMap<Character, TrieNode> children;
+    public boolean endOfWord;
+
+    public TrieNode() {
+        children = new HashMap<>();
+        endOfWord = false;
+    }
+}
+
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            if (!cur.children.containsKey(c)) {
+                cur.children.put(c, new TrieNode());
+            }
+            cur = cur.children.get(c);
+        }
+        cur.endOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            if (!cur.children.containsKey(c)) {
+                return false;
+            }
+            cur = cur.children.get(c);
+        }
+        return cur.endOfWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for (char c : prefix.toCharArray()) {
+            if (!cur.children.containsKey(c)) {
+                return false;
+            }
+            cur = cur.children.get(c);
+        }
+        return true;
+    }
+}
